57d04f58388f621df780020a5f8cfa7abc6db098
Joseph Pringle <josephpringle@unomaha.edu>
2023-04-21 16:01:57 -0500

    added documentations

--- account.py
+++ account.py
@@ -1,26 +1,48 @@
 class Account:
 
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
+    def __init__(self, name:str)-> None:
+        '''
+        function that sets up the initial variables
+        :param name: string value-account holder's name
+        '''
+        self.__account_name = name   # account name is equal to name
+        self.__account_balance = 0   # account balance is 0
 
-    def deposit(self, amount):
+    def deposit(self, amount:str)-> bool:
+        '''
+        function that adds amount to the account balance
+        :param amount: numeric value-how much money being deposited
+        :return: True if tansaction is successful and false if not
+        '''
         if amount > 0:
-            self.__account_balance += amount
+            self.__account_balance += amount   #sum of account balance and amount
             return True
         else:
             return False
 
-    def withdrawl(self, amount):
-        if amount >= 0 and amount < self.__account_balance:
+    def withdrawl(self, amount:float)-> bool:
+        '''
+        function that subtracts the amount from the account balance
+        :param amount: numeric value-how much money being withdrawn
+        :return: True if tansaction is successful and false if not
+        '''
+        if amount >= 0 and amount < self.__account_balance:   #difference between balance and amount
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_name(self):
+    def get_name(self)-> str:
+        '''
+        function that gets the account holder's name
+        :return: the account holder's name
+        '''
         return self.__account_name
 
     def get_balance(self)-> float:
+        '''
+        function that gets the balance
+        :return: the account balance
+        '''
         return float(self.__account_balance)
 
--- test_account.py
+++ test_account.py
@@ -2,21 +2,23 @@ from pytest import *
 from account import *
 
 class Test:
+
     def setup_method(self):
-        self.account1 = Account('mat')
+
+        self.account1 = Account('mat')  #name is mat
     def teardown_method(self):
         del self.account1
     def test_deposit(self):
-        assert self.account1.deposit(-2.0) is False
-        assert self.account1.deposit(0) is False
-        assert self.account1.deposit(2.0) is True
-        assert self.account1.get_balance() == 2
+        assert self.account1.deposit(-2.0) is False   #returns false when depositting -2.0
+        assert self.account1.deposit(0) is False   #returns false when depositting 0
+        assert self.account1.deposit(2.0) is True   #returns true when depositting 2.0
+        assert self.account1.get_balance() == 2   #makes sure the balance is 2
 
     def test_withdrawl(self):
-        assert self.account1.withdrawl(-2.0) is False
-        assert self.account1.withdrawl(0) is False
-        assert self.account1.get_balance() == 0
-        assert self.account1.withdrawl(200) is False
-        assert self.account1.deposit(20) is True
-        assert self.account1.withdrawl(3.0) is True
-        assert self.account1.get_balance() == 17
+        assert self.account1.withdrawl(-2.0) is False   #returns false when withdrawling -2.0
+        assert self.account1.withdrawl(0) is False   #returns false when withdrawling 0
+        assert self.account1.get_balance() == 0   #makes sure the balance is 0
+        assert self.account1.withdrawl(200) is False   #returns false when withdrawling 200
+        assert self.account1.deposit(20) is True   #returns true when depositting 20
+        assert self.account1.withdrawl(3.0) is True   #returns true when withdrawling 3.0
+        assert self.account1.get_balance() == 17   #makes sure the balance is 17
