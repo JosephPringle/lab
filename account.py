2e0474e65c97cbd1450ab37a2eb63ccc56a92faf
Joseph Pringle <josephpringle@unomaha.edu>
2023-04-21 15:09:50 -0500

    made changes to some of the functions

--- account.py
+++ account.py
@@ -1,26 +1,26 @@
 class Account:
 
-    def __init__(self, name)-> None:
-        self.__account_name = self.name
+    def __init__(self, name):
+        self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount)-> bool:
-        if self.amount >= 0:
-            self.__account_balance =+ self.amount
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdrawl(self, amount)-> bool:
-        if self.amount >= 0 and self.__account_balance:
-            self.__account_balance =- self.amount
+    def withdrawl(self, amount):
+        if amount >= 0 and amount < self.__account_balance:
+            self.__account_balance -= amount
             return True
         else:
             return False
 
-    def get_name(self)-> str:
-        return self.name
+    def get_name(self):
+        return self.__account_name
 
-    def get_amount(self)-> float:
-        return float(self.amount)
+    def get_balance(self)-> float:
+        return float(self.__account_balance)
 
